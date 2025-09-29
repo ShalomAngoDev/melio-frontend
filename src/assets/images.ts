eea20e3752da8296ee5c24f517e3a2755d8b46ec
ShalomAngoDev.ango <angoshalom@gmail.com>
2025-09-29 18:23:52 +0200

    Fix image imports: use relative paths from public folder

--- src/assets/images.ts
+++ src/assets/images.ts
@@ -1,5 +1,5 @@
 // Import des images pour éviter les problèmes de chemins sur Vercel
-import logoIcon from '/logo-icon.png';
-import fullLogo from '/fullLogo.png';
+import logoIcon from '../../public/logo-icon.png';
+import fullLogo from '../../public/fullLogo.png';
 
 export { logoIcon, fullLogo };
