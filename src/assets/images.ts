ed1f731d239a1a8130374b732a475264b719d55d
ShalomAngoDev.ango <angoshalom@gmail.com>
2025-09-29 18:26:41 +0200

    Fix image imports: use Vite ?url suffix for public assets

--- src/assets/images.ts
+++ src/assets/images.ts
@@ -1,5 +1,5 @@
 // Import des images pour éviter les problèmes de chemins sur Vercel
-import logoIcon from '../../public/logo-icon.png';
-import fullLogo from '../../public/fullLogo.png';
+import logoIcon from '/logo-icon.png?url';
+import fullLogo from '/fullLogo.png?url';
 
 export { logoIcon, fullLogo };
